a82f1f87fc4a64917494896d8a9dd91b86f13586
Bianca Delgado <valentinadelgados@icloud.com>
2025-10-01 15:46:49 -0600

    autoprefixer

---
+++ unit-3/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html", "./**/*.html"], // <- include all HTML files
+  theme: {
+    extend: {
+      colors: {
+        "brand-primary": "#526343",
+        "brand-secondary": "#B2B9AB",
+        "brand-secondary-200": "#DEE5D7",
+        "brand-tertiary": "#B4DACC",
+        "brand-hover": "#D5DDCE",
+      },
+    },
+  },
+  plugins: [],
+}
