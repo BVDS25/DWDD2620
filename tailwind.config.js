19ced88d5a9a865daec6c374813611fb99c3268e
Bianca Delgado <valentinadelgados@icloud.com>
2025-10-01 16:49:19 -0600

    Fixed tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './unit-3/index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-primary': '#526343',
+        'brand-secondary': '#B2B9AB',
+        'brand-secondary-200': '#DEE5D7',
+        'brand-tertiary': '#B4DACC',
+        'brand-hover': '#D5DDCE',
+      },
+    },
+  },
+}
